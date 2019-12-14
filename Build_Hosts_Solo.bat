fab2b24f04c605e8c99c17743ef6349c258f5cbd
thisisu <thisisu@github.com>
2019-12-13 22:01:25 -0600

    Create Build_Hosts_Solo.bat

---
+++ Build_Hosts_Solo.bat
@@ -0,0 +1,16 @@
+:: Build Hosts Solo
+:: Created by thisisu
+@echo off && SET "hostsD=C:\Windows\System32\drivers\etc"
+Echo(Build_Hosts_Solo.bat loaded!
+ECHO.
+ECHO.
+color 17
+sc config Dnscache start= disabled
+sc stop Dnscache
+NIRCMD wait 2000
+COPY /Y "E:\Github\hosts_adultxxx\hosts" "%hostsD%\hosts"
+sc config Dnscache start= auto
+SC start Dnscache
+NIRCMD WAIT 2000
+ipconfig /flushdns
+taskkill /f /im git.exe
