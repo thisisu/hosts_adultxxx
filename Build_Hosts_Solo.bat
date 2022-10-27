efe6437d367dc1613b8cf0afaad438d1d9472456
d1savow3d <35790653+thisisu@users.noreply.github.com>
2022-10-27 10:49:54 -0500

    reformat

--- Build_Hosts_Solo.bat
+++ Build_Hosts_Solo.bat
@@ -1,6 +1,6 @@
 :: Build Hosts Solo
 :: Created by thisisu
-@echo off && SET "hostsD=C:\Windows\System32\drivers\etc"
+@echo off && SET "hostsD=C:\Windows\System32\drivers\etc" && SET "githubD=C:\Users\d1savow3d\Documents\GitHub\hosts_adultxxx"
 Echo(Build_Hosts_Solo.bat loaded!
 ECHO.
 ECHO.
@@ -9,7 +9,7 @@ sc config Dnscache start= disabled
 sc stop Dnscache
 NIRCMD wait 2000
 
-GREP -vP "^#" <"E:\Github\hosts_adultxxx\hosts" >"%TEMP%\repairhosts1"
+GREP -vP "^#" <"%githubD%\hosts" >"%TEMP%\repairhosts1"
 SED -R "s/\:443$//" <"%TEMP%\repairhosts1" >"%TEMP%\repairhosts2"
 SED -R "/^0\.0\.0\.0/!s/(.*)/0\.0\.0\.0 \1/" <"%TEMP%\repairhosts2" >"%TEMP%\repairhosts3"
 SORT_ -f -u <"%TEMP%\repairhosts3" >"%TEMP%\repairhosts4"
@@ -22,7 +22,7 @@ ECHO(# 127.0.0.1 localhost>>"%TEMP%\repairhosts1"
 ECHO(# ::1 localhost>>"%TEMP%\repairhosts1"
 ECHO(# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>"%TEMP%\repairhosts1"
 TYPE "%TEMP%\repairhosts4">>"%TEMP%\repairhosts1"
-COPY /Y "%TEMP%\repairhosts1" "E:\Github\hosts_adultxxx\hosts"
+COPY /Y "%TEMP%\repairhosts1" "%githubD%\hosts"
 COPY /Y "%TEMP%\repairhosts1" "%hostsD%\hosts"
 DEL /F/Q "%TEMP%\repairhosts?" >NUL 2>&1
 sc config Dnscache start= auto
